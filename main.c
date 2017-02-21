e8b33b5fe46fea7b1ab8afb23d91685888ab530c
James Anderson <jrsa@jrsa.co>
2017-02-21 10:54:10 -0800

    enabling adc

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stm32f4xx.h> 
 
 int main() {
-    // enable GPIOA and TIM2
+    // turn on GPIOA, GPIOB, ADC1 and TIM2
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 
     GPIOA->MODER |= 0x2; // PA0 in alternate function mode
+    GPIOB->MODER |= 0x3; // PB0 in analog mode
 
     // clear pin 0 (first bit) alternate function and
     // initialize to AF01 (TIM2 CH1)
